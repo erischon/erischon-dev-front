1a028cb842c538d67aa5704714f52b0e4da7c3ab
Eri Schon <erischon@gmail.com>
2022-01-27 11:11:28 +0100

    Create blog.post.jsx

---
+++ app/src/components/blog/blog.post.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./blog.styles.css"
+
+export const Post = (props) => (
+    <main id="post">
+    { console.log(props.item)}
+        <div className="postTitle">
+            <h1>{ props.item.title }</h1>
+        </div>
+        <div className="postImage">
+            <img src="img-horizon-test-2.jpg" alt="Image de test" />
+        </div>
+        <div className="postBody">
+            <h2>Body</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ab commodi itaque assumenda natus placeat perferendis, fugiat incidunt quis cupiditate quia. Nulla illo adipisci accusamus incidunt aut possimus aspernatur veritatis.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ab commodi itaque assumenda natus placeat perferendis, fugiat incidunt quis cupiditate quia. Nulla illo adipisci accusamus incidunt aut possimus aspernatur veritatis.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ab commodi itaque assumenda natus placeat perferendis, fugiat incidunt quis cupiditate quia. Nulla illo adipisci accusamus incidunt aut possimus aspernatur veritatis.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ab commodi itaque assumenda natus placeat perferendis, fugiat incidunt quis cupiditate quia. Nulla illo adipisci accusamus incidunt aut possimus aspernatur veritatis.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ab commodi itaque assumenda natus placeat perferendis, fugiat incidunt quis cupiditate quia. Nulla illo adipisci accusamus incidunt aut possimus aspernatur veritatis.</p>
+        </div>
+        <div className="postInfos">
+            <h3>Infos</h3>
+        </div>
+        <div className="postFooter">
+            <h2>Footer</h2>
+        </div>
+    </main>
+)
+
+// <div dangerouslySetInnerHTML={{ __html: this.props.item.body }} />
